36f6f0f7cb64ee52d624c968db2936605b26b427
Xabier Ortega <xortega@c3r8s4.42urduliz.com>
2024-11-20 12:14:29 +0100

    normi casi no se queja, estandarizando texturas

--- includes/cubed.h
+++ includes/cubed.h
@@ -6,52 +6,49 @@
 /*   By: xortega <xortega@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 12:49:38 by xortega           #+#    #+#             */
-/*   Updated: 2024/11/15 13:39:48 by xortega          ###   ########.fr       */
+/*   Updated: 2024/11/20 12:03:15 by xortega          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #ifndef CUBED_H
 # define CUBED_H
 
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "structures.h"
 
-#include "../MLX42/include/MLX42/MLX42.h"
-#include "structures.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <math.h>
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
+# define MAP_SIZE 10
 
+# define MAX_DEPTH round(sqrt((10 * 10 + 10 * 10)))
 
-#define MAP_SIZE 10
+# define PI 3.14159265358979323846
 
-#define MAX_DEPTH round(sqrt((10 * 10 + 10 * 10)))
+# define PIH 1.57079632679489661923
 
-#define M_PI   3.14159265358979323846
+# define PI3H 4.71238898038468985769
 
-#define EAST 0
-#define NORTH M_PI/2 
-#define WEST M_PI
-#define SOUTH M_PI * 3/2
+# define SOUTH PI3H
+# define NORTH PIH
+# define EAST 0
+# define WEST PI
 
-#define FOV 60
+# define FOV 60
 
-#define	ANGLE_TO_RADIAN 0.0174533
+# define ANGLE_TO_RADIAN 0.0174533
 
+# define HRED	"\033[91m"
+# define HBLU	"\033[94m"
+# define HGRE	"\033[92m"
+# define HMAG	"\033[95m"
+# define RST	"\033[0m"
 
-#define HRED	"\033[91m"
-#define HBLU	"\033[94m"
-#define HGRE	"\033[92m"
-#define HMAG	"\033[95m"
-#define RST		"\033[0m"
-
-int32_t     **make_texture(mlx_texture_t* texture);
-
-void        print_walls(t_data *data);
-
-t_pair_d_p  rayo(t_data *data, double angle);
-void        ray_maker(t_data *data);
+t_texture	*make_texture(mlx_texture_t *texture);
+void		print_walls(t_data *data);
+t_pair_d_p	rayo(t_data *data, double angle);
+void		ray_maker(t_data *data);
 #endif
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,37 +6,39 @@
 /*   By: xortega <xortega@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:41:00 by xortega           #+#    #+#             */
-/*   Updated: 2024/11/14 17:41:42 by xortega          ###   ########.fr       */
+/*   Updated: 2024/11/20 12:06:49 by xortega          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
 
-int32_t make_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+int32_t	make_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-int32_t **make_texture(mlx_texture_t* texture)
+t_texture	*make_texture(mlx_texture_t *texture)
 {
-	uint32_t y;
-	uint32_t x;
-	uint32_t k;
-	int32_t **new_texture;
-	
-	new_texture = malloc(sizeof(int32_t *) * texture->height);
+	uint32_t	y;
+	uint32_t	x;
+	uint32_t	k;
+	t_texture	*new_texture;
+
+	new_texture = malloc(sizeof(t_texture));
+	new_texture->texture = malloc(sizeof(int32_t *) * texture->height);
 	for (size_t i = 0; i < texture->height; i++)
 	{
-		new_texture[i] = malloc(sizeof(int32_t) * texture->width);
+		new_texture->texture[i] = malloc(sizeof(int32_t) * texture->width);
 	}
-	
 	for (y = 0; y < texture->height; y++)
 	{
 		for (x = 0; x < texture->width; x++)
 		{
-			k = y*texture->width*4 + x*4;
-			new_texture[x][y] = make_pixel(texture->pixels[k + 0], texture->pixels[k + 1], texture->pixels[k + 2], texture->pixels[k + 3]);
+			k = y * texture->width * 4 + x * 4;
+			new_texture->texture[x][y] = make_pixel(texture->pixels[k + 0], texture->pixels[k + 1], texture->pixels[k + 2], texture->pixels[k + 3]);
 		}
 	}
-	return(new_texture);
+	new_texture->height = texture->height;
+	new_texture->width = texture->width;
+	return (new_texture);
 }
